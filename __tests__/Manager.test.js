77412acdf24c1b3aba3f0fc186344c0319df39a5
elijahb22 <elibusick11@gmail.com>
2022-01-16 20:21:52 -0500

    created test and methods for manager

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,31 +1,25 @@
-const { test } = require('picomatch');
-const Manager = require ('../lib/Manager');
+
+
+const manager= require ('../lib/Manager');
 
 //name, email, id, role and school  name validation for manager
-const managerTest = new Manager ('Elijah', 'Elijahbusick@gmail.com', 4 , 'Manager', 109)
-
-test('Manager', () => {
-    it('has a name', () =>{
+    test('has a name', () => {
+        const managerTest = new manager('Elijah', 'Elijahbusick@gmail.com', 4 , 'manager', 109);
         expect(managerTest.name).toEqual(expect.any(String))
         expect(managerTest.name.length).toBeGreaterThan(2)
-
     })
-    it('has an email id', () =>{
+
+    test('has an email id', () =>{
+        const managerTest = new manager('Elijah', 'Elijahbusick@gmail.com', 4 , 'manager', 109);
         expect(managerTest.email).toEqual(expect.stringContaining('@'))
     })
 
-    it('has an id that is a num', () => {
+    test('has an id that is a num', () => {
+        const managerTest = new manager('Elijah', 'Elijahbusick@gmail.com', 4 , 'manager', 109);
         expect(managerTest.id).toEqual(expect.any(Number))
     })
 
-    it('has a role of engineer', () => {
-        expect(managerTest.role).toBe('Manager')
-    })
-
-    it('has a office number', () => {
-        keys = Object.keys(managerTest)
-        optionKey = keys[4]
-        expect(optionKey).toBe('office')
-        expect(managerTest.office).toEqual(expect.any(Number))
-    })
-})
+    test('has a role of manager', () => {
+        const managerTest = new manager('Elijah', 'Elijahbusick@gmail.com', 4 , 'manager', 109);
+        expect(managerTest.role).toBe('manager')
+    });
